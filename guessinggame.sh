7d931edfd75ea1f83203197b76e626739af0706f
darcydang <nghidang@uab.edu>
2020-06-04 20:14:31 -0500

    added two files

---
+++ guessinggame.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+
+function guessthenumber 
+{
+	echo "How many files are in the current directory?"
+	read guess
+
+	while [[ $guess -eq 3 ]] 
+		do 
+			if [[ $guess -lt 3 ]]
+			then
+				echo "Your guess is too low. Try again."
+			elif [[ $guess -gt 3 ]]
+			then 
+				echo "Your guess is too high. Try again."
+			fi
+			
+			echo "Please guess another nummber:"
+			read guess
+		done
+	echo "Congratulations! Your guess was correct!"
+}
+
+
